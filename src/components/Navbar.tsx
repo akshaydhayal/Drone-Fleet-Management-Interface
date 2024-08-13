f5281ea6102f4a72e0c0d3e3daef80e33563e55c
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-13 18:26:05 +0530

    monor change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { GiFlyingDagger } from "react-icons/gi";
-import { LuMoonStar } from "react-icons/lu";
-import { FiSun } from "react-icons/fi";
+// import { LuMoonStar } from "react-icons/lu";
+// import { FiSun } from "react-icons/fi";
 import { useDispatch, useSelector } from 'react-redux';
 import { setLoggedUser } from '../store/loggedUserStore';
 import { RootState } from "../store/store";
@@ -19,9 +19,8 @@ const Navbar = () => {
         </p>
       </div>
       <div className='flex items-center gap-4'>
-          <LuMoonStar className='text-white h-6 w-6'/>
-          <FiSun className='text-white h-6 w-6'/>
-          {/* {!username && <p className='text-slate-200 font-medium text-lg underline'>Login please</p>} */}
+          {/* <LuMoonStar className='text-white h-6 w-6'/> */}
+          {/* <FiSun className='text-white h-6 w-6'/> */}
           {!username && <p className={`text-slate-200 font-medium text-lg underline`}>Login please</p>}
           {username && <p className='text-white'>Hi {username}</p>}
           {username && <button className='p-1 px-2 bg-sky-700 hover:bg-sky-600 rounded-md text-slate-100 border' onClick={()=>dispatch(setLoggedUser(null))}>Logout</button>}
